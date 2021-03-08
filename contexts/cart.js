752ae37a535904bb57d02f887a34c4c5ea9bc86a
ENSFELDER Lionel <ensfelder.lionel@gmail.com>
2021-03-08 19:27:21 +0100

    Add quantity to cart

--- contexts/cart.js
+++ contexts/cart.js
@@ -7,13 +7,24 @@ const cart = () => {
 
   const actions = (action) => {
     const {type, payload} = action;
-    function isProduct(id){
-      return 
+
+    const beforeAddToCart = () => {
+      let indexOfProduct = cart.findIndex(el => el.id == payload.id);
+      let tempCart = [...cart];
+
+      if(indexOfProduct < 0 ){
+        const newProduct = {...payload, quantity: 1};
+        tempCart.push(newProduct);
+        setCart([...tempCart]);
+      }else{
+        tempCart[indexOfProduct].quantity += 1;
+        setCart([...tempCart]);
+      }
     }
 
     switch(type){
       case 'addToCard':
-        setCart([...cart, payload]);
+        beforeAddToCart();
         return cart
       case 'deleteFromCard':
         const newCart = cart.filter(el => el.id !== payload.id)
