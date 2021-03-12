6d4c7af569fb0798c3034f9ed165561ed5a8c844
ENSFELDER Lionel <ensfelder.lionel@gmail.com>
2021-03-12 01:53:09 +0100

    feat(product page): add tabs & start clean design

--- pages/products/[_id].js
+++ pages/products/[_id].js
@@ -26,59 +26,123 @@ export default function Product({ product }) {
         <Navbar />
 				<div>
 					{product &&
-            <div className="uk-height-large uk-height-match uk-grid-collapse uk-child-width-1-1 uk-child-width-1-2@m" uk-grid="true">
-              <div>
-                  <div className="uk-card uk-card-body uk-text-center">
+            <div>
+
+              <div className="uk-height-match uk-grid-collapse uk-child-width-1-1 uk-child-width-1-2@m" uk-grid="true">
+                
+                <div className="uk-inline">
+                  <span className="uk-label uk-position-top-left">Default</span>
+                  <div className="uk-card uk-card-body uk-padding-remove uk-text-center">
                     <img src={product.pics[1]} width="100%" height="" alt="" uk-img="true" />
                   </div>
+                </div>
+
+                <div>
+                    <div className="uk-card uk-card-body">
+                    <div className="">
+                      <ul className="uk-breadcrumb">
+                        <li><a href="#">Accueil</a></li>
+                        <li><a href="#">{product.type}</a></li>
+                        <li><a href="#">{product.subtype}</a></li>
+                      </ul>
+                    </div>
+                      <h1 className="uk-h2 uk-text-bold uk-margin-remove-top">{product.name} {product.type} {product.brand}</h1>
+                      <span className="uk-h2 uk-text-bold uk-margin-medium-bottom uk-padding-remove-top">{product.price["$numberDecimal"]} €</span>
+                      <p className="uk-margin-medium-bottom">
+                        {product.description}<br/>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Mauris non ultrices mi. Praesent a ligula tempus...
+                      </p>
+
+                      <hr></hr>
+
+                      <div className="uk-margin-medium-top" uk-grid="true">
+
+                        <button className="uk-button uk-button-text uk-button-text-no-border" onClick={() => { quantity <= 1 ? setQuantity(1) : setQuantity(quantity - 1) } }>
+                          <i className="las la-minus la-1x"></i>
+                        </button>
+
+                        <button className="uk-button uk-button-text uk-button-text-no-border">{quantity}</button>
+
+                        <button className="uk-button uk-button-text uk-button-text-no-border" onClick={() =>{ quantity >= 5 ? setQuantity(5) : setQuantity(quantity + 1) } }>
+                          <i className="las la-plus la-1x"></i>
+                        </button>
+
+                        <button className="uk-button uk-button-secondary uk-button-large uk-margin-medium-left uk-width-expand" onClick={() => actions({type: 'addToCard', payload: product, quantity: quantity})}>
+                          <i className="las la-cart-arrow-down uk-margin-right uk-text-large"></i>
+                          Ajouter au panier
+                        </button>
+
+                      </div>
+
+                      <div className="uk-margin-top">
+
+                        <button className="uk-button uk-button-text uk-text-muted uk-button-text-no-border uk-margin-right">
+                          <i className="las la-heart uk-margin-small-right uk-text-large"></i>
+                          Ajouter au favoris
+                        </button>
+
+                        <button className="uk-button uk-button-text uk-text-muted uk-button-text-no-border">
+                          <i className="las la-share-alt uk-margin-small-right uk-text-large"></i>
+                          Partager
+                        </button>
+
+                      </div>
+
+                      <hr></hr>
+
+                      <div className="uk-margin-top">
+
+                        <p className=""><span className="uk-text-muted">En stock : </span>{ product.stocked ? 'Oui' : 'Bientôt disponible' }</p>
+                        <p className=""><span className="uk-text-muted">Référence : </span>{ product._id }</p>
+                        <p className=""><span className="uk-text-muted">Marque : </span>{product.brand}</p>
+
+                      </div>
+
+                    </div>
+                </div>
+
               </div>
-              <div>
-                  <div className="uk-card uk-card-body">
-                    <h1 className="uk-text-bold">{product.name} {product.type} {product.brand}</h1>
-                    <h2 className="uk-text-bold uk-margin-medium-bottom">{product.price["$numberDecimal"]} €</h2>
+
+              <div className="uk-margin-xlarge-top">
+
+                <div className="uk-flex uk-flex-center" uk-switcher="animation: uk-animation-fade; toggle: > *">
+
+                  <button className="uk-button uk-button-text uk-margin-right" type="button">
+                    <span className="uk-h3">Description</span>
+                  </button>
+
+                  <button className="uk-button uk-button-text" type="button">
+                    <span className="uk-h3">Avis clients</span>
+                  </button>
+
+                </div>
+
+                <ul className="uk-switcher uk-margin-medium-top">
+
+                  <li>
                     <p className="uk-margin-medium-bottom">
-                      {product.description}<br/>
+                      {product.description}
                       Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                       Mauris non ultrices mi. Praesent a ligula tempus, rhoncus arcu non, suscipit leo. 
-                      Sed cursus tortor sed nunc fringilla imperdiet.
+                      Sed cursus tortor sed nunc fringilla imperdiet.Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                      Mauris non ultrices mi.
                     </p>
+                  </li>
 
-                    <hr></hr>
+                  <li>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                      Mauris non ultrices mi. Praesent a ligula tempus, rhoncus arcu non, suscipit leo. 
+                      Sed cursus tortor sed nunc fringilla imperdiet.Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                      Mauris non ultrices mi.
+                    </p>
+                  </li>
 
-                    <div className="uk-margin-medium-top" uk-grid="true">
-                      <button className="uk-button uk-button-text" onClick={() =>{ quantity >= 5 ? setQuantity(5) : setQuantity(quantity + 1) } }>
-                        <i className="las la-plus la-2x"></i>
-                      </button>
-                      <div className="uk-text-bold uk-text-center uk-text-lead">{quantity}</div>
-                      <button className="uk-button uk-button-text uk-margin-right" onClick={() => { quantity <= 1 ? setQuantity(1) : setQuantity(quantity - 1) } }>
-                        <i className="las la-minus la-2x"></i>
-                      </button>
-                      <button className="uk-button uk-button-secondary uk-button-large" onClick={() => actions({type: 'addToCard', payload: product, quantity: quantity})}>
-                        <i className="las la-cart-arrow-down uk-margin-right uk-text-large"></i>
-                        Ajouter au panier
-                      </button>
-                    </div>
-                    <div className="uk-margin-top">
-                      <button className="uk-button uk-button-text uk-text-muted uk-margin-right">
-                        <i className="las la-bookmark uk-margin-right uk-text-large"></i>
-                        Ajouter au favoris
-                      </button>
-                      <button className="uk-button uk-button-text uk-text-muted">
-                        <i className="las la-bullhorn uk-margin-right uk-text-large"></i>
-                        Partager
-                      </button>
-                    </div>
+                </ul>
 
-                    <hr></hr>
-
-                    <div className="uk-margin-top">
-                      <p className="">En stock : { product.stocked ? 'Oui' : 'Bientôt disponible' }</p>
-                      <p className="">Référence : { product._id }</p>
-                      <p className="">Marque : {product.brand}</p>
-                    </div>
-
-                  </div>
               </div>
+
             </div>
 					}
 				</div>
