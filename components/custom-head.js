35f8b3ffe28642c1e93bd677c3346f0e19486c8c
ENSFELDER Lionel <ensfelder.lionel@gmail.com>
2021-04-26 16:22:53 +0200

    feat: change site title & add favicons

--- components/custom-head.js
+++ components/custom-head.js
@@ -3,8 +3,8 @@ import Head from 'next/head'
 export default function CustomHead(){
   return(
     <Head>
-        <title>Lezarlando</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Next.js e-shop</title>
+        <link rel="icon" href="/assets/favicon/favicon.ico" />
         <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" />
     </Head>
   )
